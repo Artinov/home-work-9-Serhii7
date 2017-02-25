951a9a60b05cfaa9ae373a8fd53bc25bcaf2b9bc
we <sergiyp777@gmail.com>
2017-02-25 16:15:11 +0200

    i have done

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 var ball = document.querySelector(".discoball");
+	var array = ["here", "goes", "our", "awesome", "disco", "ball"];
+	var i =0;
 
 function goDisco() {
-	//do your disco magic here !
+	if(i == 5){
+		i=0;
+	}
+	
+
+	ball.setAttribute('class','discoball'+ ' '+array[i]);
+	i++;
+	
 }
 
 setInterval(goDisco,300);
